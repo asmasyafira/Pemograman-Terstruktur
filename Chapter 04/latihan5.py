570d468af9a2a6f443157617b0b23e60d2dfa47b
Asma Syafira <47140064+asmasyafira@users.noreply.github.com>
2021-10-11 10:05:12 +0700

    Latihan Python

---
+++ Chapter 04/latihan5.py
@@ -0,0 +1,5 @@
+print('________Mengisi Data Jumlah Mahasiswa PTIK_________')
+jmlhLk = 42
+jmlhPr = 29
+print('Jumlah mahasiswa laki-laki PTIK UNS 2021: ' + '*' * jmlhLk + '(' + str(jmlhLk) + ')')
+print('Jumlah mahasiswa perempuan PTIK UNS 2021: ' + '*' * jmlhPr + '(' + str(jmlhPr) + ')')
