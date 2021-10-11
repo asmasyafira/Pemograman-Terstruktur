667f131a2b234d17c515868895bdeeb3e9450c73
Asma Syafira <47140064+asmasyafira@users.noreply.github.com>
2021-10-11 08:26:26 +0700

    Latihan Python

---
+++ Chapter 04/latihan2.py
@@ -0,0 +1,5 @@
+print("_________Menghitung Liter Bensin yang Diperlukan_____________")
+jarakKota = int(input("Jarak kota A ke kota C :"))
+jarakLiter = int(input("Jarak yang ditempuh per liter :"))
+totalLiter = jarakKota/jarakLiter
+print("Banyaknya bensin yang diperlukan:", totalLiter, "liter")
