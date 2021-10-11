d1f9df32512a32faf7330d2a68a6325f9d986bf4
Asma Syafira <47140064+asmasyafira@users.noreply.github.com>
2021-10-11 08:23:15 +0700

    Delete latihan2.py

--- Chapter 04/latihan2.py
+++
@@ -1,9 +0,0 @@
-Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-==================== RESTART: C:\Users\user\Downloads\no2.py ===================
-_________Menghitung Liter Bensin yang Diperlukan_____________
-Jarak kota A ke kota C : 795
-Jarak yang ditempuh per liter :12
-Banyaknya bensin yang diperlukan: 66.25 liter
->>> 
