462c6fd45cf87067f4f2f72663bfa3adfd2eb626
Asma Syafira <47140064+asmasyafira@users.noreply.github.com>
2021-11-05 10:12:38 +0700

    Add files via upload

---
+++ Chapter 07/Latihan/latihan-2.py
@@ -0,0 +1,15 @@
+nameFile = input("Masukkan nama file: ")
+
+try:
+    file = open(nameFile, "a")
+    file.write(input("Data yang mau ditambahkan: "))
+    tambah = input("Mau lagi (y/n) : ")
+    while (tambah == 'y'):
+        file.write(input("Data yang mau ditambahkan: "))
+        Add = input("Mau lagi (y/n) : ")
+        if (Add == 'n'):
+            file.close()
+except FileNotFoundError:
+    print("File Tidak Ada")
+except ValueError:
+    print('Input tidak valid')
