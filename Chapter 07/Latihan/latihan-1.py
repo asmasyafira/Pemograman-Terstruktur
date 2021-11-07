283b9c74ba83783761ed3f980a0a13b218cece73
Asma Syafira <47140064+asmasyafira@users.noreply.github.com>
2021-11-07 10:24:26 +0700

    Delete latihan-1.py

--- Chapter 07/Latihan/latihan-1.py
+++
@@ -1,8 +0,0 @@
-nameFile = input("Masukkan nama file:")
-
-try:
-    file = open(nameFile, 'r')
-    print("Isi file", nameFile, "adalah", file.read())
-except:
-    print("File tidak ada / salah penulisan")
-
